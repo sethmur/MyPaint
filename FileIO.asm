08d1d42f241c28ceef502df0594be8eb0807b416
Ruvim Lashchuk <ruvimlashchuk25@gmail.com>
2017-12-03 13:36:45 -0800

    FileIO_IntermediateCommit_V2
    Operational, but it doesn't produce the file we need.

--- FileIO.asm
+++ FileIO.asm
@@ -1,25 +1,24 @@
-	;=======================================================================================================;
-	;	Authors: Erik Olson, Nathan Hoffman, Ruvim Lashchuk, and Seth Murdoch								;	
-	;	Date: 2017-12-02
-	;
-	;	This program takes "inFile.bmp" from it's directory and manipulated the RGB values of each			;
-	;		pixel in the image. The code isn't dynamic.  We hardcoded 24-bit bmp file types.				;
-	;		Making the bmp type dynamic would require changing some hardcoded values to defined variables.	;
-	;	
-	;	Relevant links:																						;
-	;		CreateFile:
-	;			https://msdn.microsoft.com/en-us/library/windows/desktop/aa363858(v=vs.85).aspx
-	;		ReadFile:
-	;			https://msdn.microsoft.com/en-us/library/windows/desktop/aa365467(v=vs.85).aspx
-	;		WriteFile:
-	;			https://msdn.microsoft.com/en-us/library/windows/desktop/aa365747(v=vs.85).aspx
-	;		ExitProcess:
-	;			https://msdn.microsoft.com/en-us/library/windows/desktop/ms682658(v=vs.85).aspx
-	;		BMP Format:
-	;			http://www.fastgraph.com/help/bmp_header_format.html
-	;			https://upload.wikimedia.org/wikipedia/commons/c/c4/BMPfileFormat.png
-	;			http://www.daubnet.com/en/file-format-bmp
-	;==================================================================================================
+	;===================================================================================================================;
+	;	Authors: Erik Olson, Nathan Hoffman, Ruvim Lashchuk, and Seth Murdoch											;	
+	;	Date: 2017-12-02																								;
+	;																													;
+	;	This program takes "inFile.bmp" from its directory and manipulates the RGB values of each pixel in the image.	;
+	;		The code isn't dynamic. We hardcoded it to only work with 24-bit bmp file types.							;
+	;																													;
+	;	Relevant links:																									;
+	;		CreateFile:																									;
+	;			https://msdn.microsoft.com/en-us/library/windows/desktop/aa363858(v=vs.85).aspx							;
+	;		ReadFile:																									;
+	;			https://msdn.microsoft.com/en-us/library/windows/desktop/aa365467(v=vs.85).aspx							;
+	;		WriteFile:																									;
+	;			https://msdn.microsoft.com/en-us/library/windows/desktop/aa365747(v=vs.85).aspx							;
+	;		ExitProcess:																								;
+	;			https://msdn.microsoft.com/en-us/library/windows/desktop/ms682658(v=vs.85).aspx							;
+	;		BMP Format:																									;
+	;			http://www.fastgraph.com/help/bmp_header_format.html													;
+	;			https://upload.wikimedia.org/wikipedia/commons/c/c4/BMPfileFormat.png									;
+	;			http://www.daubnet.com/en/file-format-bmp																;
+	;===================================================================================================================;
 	
 	.486                                    ; create 32 bit code
     .model flat, stdcall                    ; 32 bit memory model
@@ -38,13 +37,12 @@
     includelib \masm32\lib\user32.lib
     includelib \masm32\lib\kernel32.lib
 
-	include io.h
 
-	;===================================================================================;
-	;	Handling 24bit bmp files														;
-	;	Header: 54 bytes																;
-	;	Padding: Each row is padded to be a multiple of 4 bytes. Range = 0 to 3 bytes.	;
-	;===================================================================================;
+	;=======================================================================================;
+	;		Handling 24bit bmp files														;
+	;		Header: 54 bytes																;
+	;		Padding: Each row is padded to be a multiple of 4 bytes. Range = 0 to 3 bytes.	;
+	;=======================================================================================;
 
     .data
     	
@@ -55,19 +53,24 @@
 		outFile				BYTE	"outFile.bmp", 0
 
 		readBytes			DWORD	?				; stores how many bytes were read from file
-		pixelArray			BYTE	100000 DUP (?)
-		pixelArray_Size		DWORD	?
+		
+		; Temporarily chaning byte value to 3126 BYTEs for 32 x 32 resolution to make debugging more efficient.  
+		pixelArray			BYTE	3126 DUP (?)	; Imposing a resolution limit of 1920 x 1080.  6220800 = 1920*1080*3
+		pixelArray_Size		DWORD	?				; Dynamic element.  Parsed from bmp header.
+		iPixel				BYTE	?, ?, ?			; Individial BRG pixel that will be scaled and stored. Short for "Individual Pixel".
+		
 		bmpHeader			BYTE	54 DUP (?)		; Standard bmp file header size
 		bmpHeader_Size		DWORD	54				; To bypass image header
+		
 		xPixels				DWORD	?				; Offset of 18 bytes
 		yPixels				DWORD	?				; Offset of 22 bytes
 		padBytes			DWORD	?				; xPixels % 4
+		
 		index				DWORD	?				; = (row * xPixel + column) * 3 + row * padding
-		sPixel				DWORD	?				; Individial pixel that will be scaled and stored
 
     .code                       ; Tell MASM where the code starts
 
-; «««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
+; Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«
 
 start:                          ; The CODE entry point to the program
 
@@ -75,16 +78,21 @@ start:                          ; The CODE entry point to the program
 
     exit
 
-; «««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
+; Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«
 
 main proc
-	
-	;===================================================================;
-	;		Using CreateFile to get File handle for our output file		;	
-	;===================================================================;
+	xor		eax, eax
+	xor		ebx, ebx
+	xor		ecx, ecx
+	xor		edx, edx
+
+
+	;=======================================================================;
+	;		Using CreateFile to get File handle for our output file			;	
+	;=======================================================================;
     ; CreateFile(lpFileName, dwDesiredAccess, dwShareMode, lpSecurityAttributes, dwCreationDisposition, dwFlagsAndAttributes, hTemplateFile)
 	invoke	CreateFile, offset outFile, GENERIC_WRITE, 0, 0, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0
-	move	hFileOut, EAX	; Move file handle from common register for file to output image
+	mov		hFileOut, EAX	; Move file handle from common register for file to output image
 
 
 
@@ -97,27 +105,31 @@ main proc
 
 
 
-	;=====================================================;=========================================================;
-	;  Reading file data.																							;
-	;		Step 1:	parsing file header for file information														;
-	;		step 2:	reading the rest of the information separately to store only the pixel content in pixelArray.	;
-	;				See fastgraph.com/help/bmp_header_format.html													;
-	;===============================================================================================================;
+	;===================================================================================================================;
+	;		Reading file data.																							;
+	;			Step 1:	parsing file header for file information														;
+	;			step 2:	reading the rest of the information separately to store only the pixel content in pixelArray.	;
+	;					See fastgraph.com/help/bmp_header_format.html													;
+	;===================================================================================================================;
 	; ReadFile(hFile, lpBuffer, nNumberOfBytesToRead, lpNumberOfBytesToRead, lpOverlapped)
-	invoke	ReadFile, hFile, offset bmpHeader, bmpHeader_FileSize, readBytes, 0
+	invoke	ReadFile, hFile, offset bmpHeader, bmpHeader_Size, readBytes, 0
 	
-	; DWORD because the width value is stored in four bytes
-	mov		edx, DWORD PTR [bmpHeader + 18]	; +18 corresponds to width in pixels
-	mov		xPixels, edx					
 
-	mov		edx, DWORD PTR [bmpHeader + 22]	; +22 corresponds to height in pixels
-	mov		yPixels, edx										
+	; DWORD PTR call isn't formatted properly.  Should be DWORD PTR bmpHeader[#], but I want to read up on this.  See chapter 5 for more info.
+	; DWORD because the width value is stored in four bytes
+	; Hexadecimal representation in memory
+	mov		eax, DWORD PTR bmpHeader[22]	; +22 corresponds to height in pixels			
+	mov		yPixels, eax					; Moving double word in eax into yPixels
+	
+	mov		eax, DWORD PTR bmpHeader[18]	; +18 corresponds to width in pixels
+	mov		xPixels, eax					; Moving double word in eax into yPixels
+
 
 	; Finding padding of the rows
-	; xor edx, edx  Might need this
-	mov		eax, xPixels					
-	mov		ebx, 4
-	div		ebx								; divide eax(xPixels) by 4
+	xor		edx, edx						; Clearing EDX register to prevent overflow and undesired data.
+	; eax currently holds xPixels
+	mov		ebx, 4				
+	div		ebx								; divide eax(xPixels) by 4	; Integer overflow
 	mov		padBytes, edx					; edx holds remainder
 
 	mov		edx, DWORD PTR [bmpHeader + 2]	; +2 corresponds to file size
@@ -127,64 +139,92 @@ main proc
 
 	; Reading the rest of the file
 	; ReadFile(hFile, lpBuffer, nNumberOfBytesToRead, lpNumberOfBytesToRead, lpOverlapped)
-	invoke	ReadFile hFile, offset pixelArray, pixelArray_Size, readBytes, 0
+	invoke	ReadFile, hFile, offset pixelArray, pixelArray_Size, readBytes, 0
+
+
+
+	;===================================================================================;
+	;		Writing bmp header to file.  The header's information is preserved.			;
+	;				The program only manipulates pixel information.						;
+	;===================================================================================;
+	; WriteFile(hFile, lpBuffer, nNumberOfBytesToWrite, lpNumberOfBytesWritten, lpOverlapped)
+	invoke WriteFile, hFileOut, offset bmpHeader, 54, offset hFileOut, 0
+	; cmp		eax, 0
+	; jz		endRowLoop						; WriteFile returns true for success.  If it fails here there is no reason to continue.
 
 
 
 	;===================================================================================;
 	;		Looping through the Pixel Array space and manipulating every RGB pixel.		;
 	;===================================================================================;
-	mov		edx, yPixel						; Count of rows 
-	dec		yPixel							; Range is 0 to yPixel - 1
+	xor		edx, edx						; RowCounter: Initialiaze to 0
+											; Range: 0 to (yPixels - 1)			= Rows
 
+
+	; RowLoop isn't necessary, but it makes the process more intuitive for an image.  
+	; If we wanted to optimize the program we would do away with RowLoop, and have the entire algorithm iterate pixel by pixel
 RowLoop:
-	xor		ecx, ecx						; Count of Columns, starting at 0 for every iteration of our outer loop
+	xor		ecx, ecx						; ColumnCounter,: Initialize to 0
+											; Domain: 0 to xPixel - 1			= Columns
 
 ColumnLoop:
-	; Calculating the offset from buffer
+	;=======================================================================================================================;
+	;							The file contains bmpHeader at byte position 0 through 53 									;
+	;		Index = ( Pixel.Row * Row.Width + Pixel.Column ) * Pixel.Size + Pixel.Row * Row.Padding + bmpHeader_Size		;
+	;=======================================================================================================================;
+	xor		eax, eax						; Initialize Registers to clear old data
+	xor		ebx, ebx
+	
 	mov		eax, edx						; row
-	mul		xPixel							; row * xPixel
-	add		eax, ecx						; row * xPixel + ecx
-	mul		3								; (row * xPixel + ecx) * 3
-	mov		index, eax						
-	mov		eax, padBytes					; padBytes
-	mul		edx								; padBytes * edx
-	add		index, eax						; (row * xPixel + ecx) * 3 + padBytes * edx
+	mul		xPixels							; row * xPixels
+	add		eax, ecx						; row * xPixels + ecx
+	imul	eax, 3							; (row * xPixels + ecx) * 3			; In 24-bit bmp: Pixel.Size = 3
+	mov		ebx, padBytes					; padBytes
+	imul	ebx, edx						; padBytes * edx
+	add		ebx, bmpHeader_Size-1			; padBytes * edx + 53
 
-	;===============================================;
-	;		Retrieving the bit RGB24 values			;
-	;			3 Bytes: Red, Green Blue			;
-	;===============================================;
+	; Complete Index
+	add		eax, ebx						; [(row * xPixels + ecx) * 3] + [padBytes * edx + 53]
+	mov		index, eax
+
+
+	;===========================================================;
+	;			Retrieving the bit RGB24 values					;
+	;				3 Bytes: Red, Green Blue					;
+	;		pixelArray starts at bottom left of the image		;
+	;	  In sets of three bytes, the color order is: BRG		;
+	;===========================================================;
 	lea		eax, pixelArray					; load effective address of pixelArray					
 	add		eax, index						; move to address of the pixel of interest
 
-	; mozx pads unused memory with zeros. We are using 32 bit registers and storing 8-bit values
-	movzx	ebx, BYTE PTR [eax]				; Moving Red value to ebx		
-	
-	movzx	ebx, BYTE PTR [eax + 1]			; Moving Green value to ebx
+	; Instruction operands must be the same size so we're using lower registers
+	mov		bl, BYTE PTR [eax]				; Moving Blue value to ebx		
+	mov		iPixel, bl
 
-	movzx	ebx, BYTE PTR [eax + 2]			; Moving Blue value to ebx
-	
+	mov		bl, BYTE PTR [eax + 1]			; Moving Green value to ebx
+	mov		iPixel[1], bl
 
-	;=========================================;
-	;		Saving pixel to memory			  ;
-	;=========================================;
+	mov		bl, BYTE PTR [eax + 2]			; Moving Red value to ebx
+	mov		iPixel[2], bl
+
+
+	;=======================================;
+	;		Saving pixel to memory			;
+	;			Not operational(?)			;
+	;=======================================;
 	mov		eax, offset hFileOut
 	add		eax, index
 	; WriteFile(hFile, lpBuffer, nNumberOfBytesToWrite, lpNumberOfBytesWritten, lpOverlapped)
-	invoke WriteFile, hFileOut, offset sPixel, 3, eax, 0
+	invoke WriteFile, hFileOut, offset iPixel, 3, eax, 0
 
-	; increment oute
+	; increment ColumnCounter
 	inc		ecx
 	cmp		ecx, xPixels
-	jl		ColumnLoop
+	jge		ColumnLoop
 endColumnLoop:
 
-	; write 13 to file,		0D or 13 is carriage return
-	; write 10 to file,		0A or 10 is line feed
-
-	dec		edx
-	cmp		edx, 0
+	inc		edx
+	cmp		edx, yPixels
 	jge		RowLoop
 
 endRowLoop:
@@ -195,6 +235,6 @@ endRowLoop:
 	ret
 main endp
 
-; «««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««««
+; Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«Â«
 
 end start                       ; Tell MASM where the program ends
